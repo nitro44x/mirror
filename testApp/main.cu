0f279eb00a214dd4ff457a3f644e33175a14e244
Chris <cpetrovitch@gmail.com>
2019-01-12 19:40:53 -0500

    some cleanup and added managed type for easier unified memory usage.

--- testApp/main.cu
+++ testApp/main.cu
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 
 #include <vector>
 #include <numeric>
@@ -16,44 +17,73 @@ void assert_(cudaError_t code, const char *file, int line) {
 
 #ifdef __CUDACC__
 #define HOSTDEVICE __host__ __device__
+#define HOST __host__
+#define DEVICE __device__
 #else
 #define HOSTDEVICE
+#define HOST
+#define DEVICE
 #endif
 
 namespace simt {
+
+	/*
+		https://stackoverflow.com/a/53033942
+		Broadly speaking, an Allocator type is used when an object of one type (typically a container) needs to 
+		manage memory to hold an object or objects of some other type. Overloading operator new and operator delete 
+		within a class is used when objects of that type need some special memory management. 
+	*/
 
 	template <class T>
 	struct managed_allocator {
 		typedef std::size_t size_type;
 		typedef std::ptrdiff_t difference_type;
 													   
-		typedef T value_type;
-		typedef T* pointer;// (deprecated in C++17)(removed in C++20)	T*
-		typedef const T* const_pointer;// (deprecated in C++17)(removed in C++20)	const T*
-		typedef T& reference;// (deprecated in C++17)(removed in C++20)	T&
-		typedef const T& const_reference;// (deprecated in C++17)(removed in C++20)	const T&
+		using value_type = T;
+		using pointer = T * ;
+		using const_pointer = const T*;
+		using reference = T & ;
+		using const_reference = const T&;
 																			   
 		template< class U > struct rebind { typedef managed_allocator<U> other; };
 		managed_allocator() = default;
 
 		template <class U> constexpr managed_allocator(const managed_allocator<U>&) noexcept {}
 
-		T* allocate(std::size_t n) {
+		HOST T* allocate(std::size_t n) {
+			//static_assert(!std::is_trivially_copyable<T>::value, "Managed Allocator can only be used for trivially copiable types");
+
 			void* out = nullptr;
 			check(cudaMallocManaged(&out, n * sizeof(T)));
+			memset(out, 0, n * sizeof(T));
 			return static_cast<T*>(out);
 		}
 
-		void deallocate(T* p, std::size_t) noexcept {
+		HOST void deallocate(T* p, std::size_t) noexcept {
 			check(cudaFree(p));
 		}
 
 	};
 
+	class Managed {
+	public:
+		void *operator new(size_t len) {
+			void *ptr;
+			cudaMallocManaged(&ptr, len);
+			cudaDeviceSynchronize();
+			return ptr;
+		}
+
+		void operator delete(void *ptr) {
+			cudaDeviceSynchronize();
+			cudaFree(ptr);
+		}
+	};
+
 	namespace containers {
 
 		template <typename T, class Alloc = simt::managed_allocator<T>>
-		class vector {
+		class vector : public Managed {
 		public:
 
 			class vectorIter;
@@ -92,6 +122,7 @@ namespace simt {
 			};
 
 		public:
+			vector() = default;
 			vector(size_type nElements) : m_alloc(), m_data(m_alloc.allocate(nElements)), m_size(nElements), m_capacity(nElements) {}
 
 			vector(size_type nElements, value_type initValue) : vector(nElements) {
@@ -103,12 +134,23 @@ namespace simt {
 				m_alloc.deallocate(m_data, m_size);
 			}
 
-			void resize(size_type nElements) {
-				pointer tmp = m_alloc.allocate(nElements);
-				memcpy(tmp, m_data, nElements > m_size ? m_size : nElements);
-				m_data = tmp;
+			HOST void resize(size_type nElements) {
+				if (nElements > capacity()) {
+					pointer tmp = m_alloc.allocate(nElements);
+					memcpy(tmp, m_data, sizeof(T) * size());
+					m_data = tmp;
+					m_capacity = nElements;
+				}
+				
 				m_size = nElements;
-				m_capacity = nElements;
+			}
+
+			HOST void push_back(value_type value) {
+				auto const currentSize = size();
+				if (currentSize <= capacity())
+					grow();
+				this->operator[](currentSize) = value;
+				++m_size;
 			}
 
 			HOSTDEVICE pointer data() const { return m_data; }
@@ -116,21 +158,40 @@ namespace simt {
 			HOSTDEVICE size_type capacity() const { return m_capacity; }
 
 			HOSTDEVICE reference operator[](size_type index) { return *(m_data + index); }
+			HOSTDEVICE const_reference operator[](size_type index) const { return *(m_data + index); }
 
 			HOSTDEVICE iterator begin() { return iterator(*this, 0); }
 			HOSTDEVICE iterator end() { return iterator(*this, m_size); }
 
 		private:
-			allocator_type m_alloc;
-			pointer m_data;
-			size_type m_size;
-			size_type m_capacity;
+			void grow() {
+				auto const newCapacity = capacity() * 2;
+				pointer tmp = m_alloc.allocate(newCapacity);
+				memcpy(tmp, m_data, sizeof(T) * size());
+				m_data = tmp;
+				m_capacity = newCapacity;
+			}
 
-			//friend class iterator;
+			allocator_type m_alloc{};
+			pointer m_data = nullptr;
+			size_type m_size = 0;
+			size_type m_capacity = 0;
+		};
+	}
+
+	namespace management {
+		
+		class Store {
+		public:
+
+		private:
+
+
 		};
 	}
 }
 
+using managed_vector = simt::containers::vector<double, simt::managed_allocator<double>>;
 
 template <typename T>
 __global__ void printArray(T* data, size_t size) {
@@ -142,25 +203,38 @@ __global__ void printArray(T* data, size_t size) {
 	}
 }
 
-template <typename VectorContainer>
-__host__ __device__ void printVector(VectorContainer * v) {
+HOSTDEVICE void printVector(managed_vector * v) {
 	printf("gpu v = ");
 	for (auto const& d : *v)
 		printf("%lf ", d);
 	printf("\n");
 }
 
-template <typename VectorContainer>
-__global__ void call_printVector(VectorContainer * v) {
+__global__ void call_printVector(managed_vector * v) {
 	if (threadIdx.x == 0 && blockIdx.x == 0) {
 		printVector(v);
 	}
 }
 
-//template <typename T>
-//__global__ void muckWithVector(simt::containers::vector)
+__global__ void call_printVector_ref(managed_vector & v) {
+	if (threadIdx.x == 0 && blockIdx.x == 0) {
+		printVector(&v);
+	}
+}
 
-int main() {
+
+HOSTDEVICE void setTo(managed_vector * v, managed_vector::value_type value) {
+	for (auto & d : *v)
+		d = value;
+}
+
+__global__ void call_setTo(managed_vector * v, managed_vector::value_type value) {
+	if (threadIdx.x == 0 && blockIdx.x == 0) {
+		setTo(v, value);
+	}
+}
+
+void test1() {
 	std::cout << "std::vector" << std::endl;
 	std::vector<double, simt::managed_allocator<double>> v(10);
 	std::iota(begin(v), end(v), -4);
@@ -169,13 +243,16 @@ int main() {
 		std::cout << d << " ";
 	std::cout << std::endl;
 
-	printArray<<<1,1>>>(v.data(), v.size());
+	printArray<<<1, 1>>>(v.data(), v.size());
 	cudaDeviceSynchronize();
 	std::cout << std::endl;
+}
 
+void test2() {
 	std::cout << "simt::containers::vector [raw ptr]" << std::endl;
 	simt::containers::vector<double, simt::managed_allocator<double>> simt_v(10, 3.0);
 	std::iota(simt_v.begin(), simt_v.end(), -3.0);
+	simt_v.push_back(4321);
 	std::cout << "cpu v = ";
 	for (auto const& d : simt_v)
 		std::cout << d << " ";
@@ -183,12 +260,11 @@ int main() {
 	printArray<<<1,1>>>(simt_v.data(), simt_v.size());
 	cudaDeviceSynchronize();
 	std::cout << std::endl;
-	
+}
+
+void test3() {
 	std::cout << "simt::containers::vector [object]" << std::endl;
-	typedef simt::containers::vector<double, simt::managed_allocator<double>> managed_vector;
-	simt::managed_allocator<managed_vector> alloc;
-	auto simt_v_ptr = alloc.allocate(1);
-	simt_v_ptr->resize(10);
+	auto simt_v_ptr = new managed_vector(10);
 	std::iota(simt_v_ptr->begin(), simt_v_ptr->end(), -4);
 	std::cout << "cpu v = ";
 	for (auto const& d : *simt_v_ptr)
@@ -197,11 +273,66 @@ int main() {
 	printVector(simt_v_ptr);
 	call_printVector<<<1,1>>>(simt_v_ptr);
 	cudaDeviceSynchronize();
-	simt_v_ptr->~vector();
-	alloc.deallocate(simt_v_ptr, 0);
+	delete simt_v_ptr;
 	std::cout << std::endl;
+}
 
-	
-    
+void test3a() {
+	std::cout << "simt::containers::vector [object] printByRef" << std::endl;
+	auto simt_v_ptr = new managed_vector(10);
+	std::iota(simt_v_ptr->begin(), simt_v_ptr->end(), -4);
+	std::cout << "cpu v = ";
+	for (auto const& d : *simt_v_ptr)
+		std::cout << d << " ";
+	std::cout << std::endl;
+	printVector(simt_v_ptr);
+	call_printVector_ref<<<1,1>>>(*simt_v_ptr);
+	cudaDeviceSynchronize();
+	delete simt_v_ptr;
+	std::cout << std::endl;
+}
+
+void test4() {
+	std::cout << "modify simt::containers::vector [object] on cpu" << std::endl;
+	auto simt_v_ptr = new managed_vector;
+	simt_v_ptr->resize(10);
+	setTo(simt_v_ptr, 123);
+	std::cout << "cpu v = ";
+	for (auto const& d : *simt_v_ptr)
+		std::cout << d << " ";
+	std::cout << std::endl;
+	call_printVector<<<1,1>>>(simt_v_ptr);
+	cudaDeviceSynchronize();
+	delete simt_v_ptr;
+	std::cout << std::endl;
+}
+
+void test5() {
+	std::cout << "modify simt::containers::vector [object] on gpu" << std::endl;
+	auto simt_v_ptr = new managed_vector;
+	simt_v_ptr->resize(10);
+	call_setTo<<<1,1>>>(simt_v_ptr, 123);
+	cudaDeviceSynchronize();
+	std::cout << "cpu v = ";
+	for (auto const& d : *simt_v_ptr)
+		std::cout << d << " ";
+	std::cout << std::endl;
+	call_printVector<<<1,1>>>(simt_v_ptr);
+	cudaDeviceSynchronize();
+	delete simt_v_ptr;
+	std::cout << std::endl;
+}
+
+int main() {
+	// Print vector tests
+	test1();
+	test2();
+	test3();
+	test3a();
+
+	// Modify vector tests
+	test4();
+	test5();
+
     return EXIT_SUCCESS;
 }
