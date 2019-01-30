54868eca08d98668522d30212ff5b61f34321759
Chris <cpetrovitch@gmail.com>
2019-01-29 22:49:20 -0500

    Added manual sync and grouping by Warp benchmarks

--- examples/particleSim/particleSim.cpp
+++ examples/particleSim/particleSim.cpp
@@ -27,9 +27,9 @@ using namespace Catch;
  *
  */
 
+template <typename Real = double, typename Alloc = mirror::managed_allocator<Real>>
 class DataStore : public mirror::Managed {
-    using Real = double;
-    using vector_t = mirror::managed_vector<Real>;
+    using vector_t = mirror::vector<Real, Alloc>;
 
 public:
     HOST DataStore(size_t nParticles) : m_nParticles(nParticles), 
@@ -61,13 +61,38 @@ public:
         std::normal_distribution<double> norm_pos(0, 1);
         std::normal_distribution<double> norm_vel(0, 10);
 
-        for (size_t i = 0; i < m_nParticles; ++i) {
-            m_x[i] = norm_pos(engine);
-            m_y[i] = norm_pos(engine);
-            m_z[i] = norm_pos(engine) + 10;
-            m_Vx[i] = norm_vel(engine);
-            m_Vy[i] = norm_vel(engine);
-            m_Vz[i] = norm_vel(engine) + 5;
+        if (!std::is_same<Alloc, mirror::device_allocator<Real>>::value) {
+            for (size_t i = 0; i < m_nParticles; ++i) {
+                m_x[i] = norm_pos(engine);
+                m_y[i] = norm_pos(engine);
+                m_z[i] = norm_pos(engine) + 10;
+                m_Vx[i] = norm_vel(engine);
+                m_Vy[i] = norm_vel(engine);
+                m_Vz[i] = norm_vel(engine) + 5;
+            }
+        }
+        else {
+            std::vector<Real> x(m_x->size());
+            std::vector<Real> y(m_y->size());
+            std::vector<Real> z(m_z->size());
+            std::vector<Real> Vx(m_Vx->size());
+            std::vector<Real> Vy(m_Vy->size());
+            std::vector<Real> Vz(m_Vz->size());
+            for (size_t i = 0; i < m_nParticles; ++i) {
+                x[i] = norm_pos(engine);
+                y[i] = norm_pos(engine);
+                z[i] = norm_pos(engine) + 10;
+                Vx[i] = norm_vel(engine);
+                Vy[i] = norm_vel(engine);
+                Vz[i] = norm_vel(engine) + 5;
+            }
+
+            mirror_check(cudaMemcpy(m_x->data(), x.data(), sizeof(Real) * x.size(), cudaMemcpyHostToDevice));
+            mirror_check(cudaMemcpy(m_y->data(), y.data(), sizeof(Real) * y.size(), cudaMemcpyHostToDevice));
+            mirror_check(cudaMemcpy(m_z->data(), z.data(), sizeof(Real) * z.size(), cudaMemcpyHostToDevice));
+            mirror_check(cudaMemcpy(m_Vx->data(), Vx.data(), sizeof(Real) * Vx.size(), cudaMemcpyHostToDevice));
+            mirror_check(cudaMemcpy(m_Vy->data(), Vy.data(), sizeof(Real) * Vy.size(), cudaMemcpyHostToDevice));
+            mirror_check(cudaMemcpy(m_Vz->data(), Vz.data(), sizeof(Real) * Vz.size(), cudaMemcpyHostToDevice));
         }
     }
 
@@ -84,13 +109,36 @@ public:
 
     HOST std::vector<double> snapshot() const {
         std::vector<double> s(m_x->size() * 6);
-        for (size_t i = 0; i < m_nParticles; ++i) {
-            s[6 * i + 0] = m_x[i];
-            s[6 * i + 1] = m_y[i];
-            s[6 * i + 2] = m_z[i];
-            s[6 * i + 3] = m_Vx[i];
-            s[6 * i + 4] = m_Vy[i];
-            s[6 * i + 5] = m_Vz[i];
+        if (!std::is_same<Alloc, mirror::device_allocator<Real>>::value) {
+            for (size_t i = 0; i < m_nParticles; ++i) {
+                s[6 * i + 0] = m_x[i];
+                s[6 * i + 1] = m_y[i];
+                s[6 * i + 2] = m_z[i];
+                s[6 * i + 3] = m_Vx[i];
+                s[6 * i + 4] = m_Vy[i];
+                s[6 * i + 5] = m_Vz[i];
+            }
+        }
+        else {
+            std::vector<Real> x(m_x->size());
+            std::vector<Real> y(m_y->size());
+            std::vector<Real> z(m_z->size());
+            std::vector<Real> Vx(m_Vx->size());
+            std::vector<Real> Vy(m_Vy->size());
+            std::vector<Real> Vz(m_Vz->size());
+
+            size_t offset = 0;
+            mirror_check(cudaMemcpy(s.data() + offset, m_x->data(), sizeof(Real) * x.size(), cudaMemcpyDeviceToHost));
+            offset += m_x->size();
+            mirror_check(cudaMemcpy(s.data() + offset, m_y->data(), sizeof(Real) * y.size(), cudaMemcpyDeviceToHost));
+            offset += m_x->size();
+            mirror_check(cudaMemcpy(s.data() + offset, m_z->data(), sizeof(Real) * z.size(), cudaMemcpyDeviceToHost));
+            offset += m_x->size();
+            mirror_check(cudaMemcpy(s.data() + offset, m_Vx->data(), sizeof(Real) * Vx.size(), cudaMemcpyDeviceToHost));
+            offset += m_x->size();
+            mirror_check(cudaMemcpy(s.data() + offset, m_Vy->data(), sizeof(Real) * Vy.size(), cudaMemcpyDeviceToHost));
+            offset += m_x->size();
+            mirror_check(cudaMemcpy(s.data() + offset, m_Vz->data(), sizeof(Real) * Vz.size(), cudaMemcpyDeviceToHost));
         }
         return std::move(s);
     }
@@ -106,9 +154,30 @@ private:
     mirror::MaybeOwner<vector_t> m_Vz;
 };
 
+#define STRIDE_64K 65536
+template <typename Real, typename Alloc>
+__global__ void call_integrateTo_byWarp(double dt, mirror::managed_vector<Particle*> const* particles, DataStore<Real, Alloc> * store) {
+    int lane_id = threadIdx.x & 31;
+    size_t warp_id = (threadIdx.x + blockIdx.x * blockDim.x) >> 5;
+    int warps_per_grid = (blockDim.x * gridDim.x) >> 5;
+    auto const size = particles->size();
+    size_t warp_total = (size + STRIDE_64K - 1) / STRIDE_64K;
 
-template <typename ParticleContainer>
-void integrateTo_cpu(double dt, ParticleContainer const* particles, DataStore * store) {
+    size_t n = size / sizeof(Real);
+
+    for (; warp_id < warp_total; warp_id += warps_per_grid) {
+        #pragma unroll
+        for (int rep = 0; rep < STRIDE_64K / sizeof(Real) / 32; rep++) {
+            size_t ind = warp_id * STRIDE_64K / sizeof(Real) + rep * 32 + lane_id;
+            if (ind < n) {
+                integrateTo(dt, particles, store, ind);
+            }
+        }
+    }
+}
+
+template <typename ParticleContainer, typename Real, typename Alloc>
+void integrateTo_cpu(double dt, ParticleContainer const* particles, DataStore<Real, Alloc> * store) {
 
     #pragma omp parallel for
     for (int i = 0; i < particles->size(); ++i) {
@@ -116,8 +185,8 @@ void integrateTo_cpu(double dt, ParticleContainer const* particles, DataStore * 
     }
 }
 
-template <typename ParticleContainer>
-DEVICE void integrateTo_gpu(double dt, ParticleContainer const* particles, DataStore * store) {
+template <typename ParticleContainer, typename Real, typename Alloc>
+DEVICE void integrateTo_gpu(double dt, ParticleContainer const* particles, DataStore<Real, Alloc> * store) {
     auto tid = mirror::getTID();
     auto stride = mirror::gridStride();
 
@@ -126,8 +195,8 @@ DEVICE void integrateTo_gpu(double dt, ParticleContainer const* particles, DataS
     }
 }
 
-template <typename ParticleContainer>
-HOSTDEVICE void integrateTo(double dt, ParticleContainer const* particles, DataStore * store, size_t tid) {
+template <typename ParticleContainer, typename Real, typename Alloc>
+HOSTDEVICE void integrateTo(double dt, ParticleContainer const* particles, DataStore<Real, Alloc> * store, size_t tid) {
     double const rho = 1.0;
     double const g = -9.8;
 
@@ -168,11 +237,13 @@ HOSTDEVICE void integrateTo(double dt, ParticleContainer const* particles, DataS
     }
 }
 
-__global__ void call_integrateTo(double dt, mirror::managed_vector<Particle*> const* particles, DataStore * store) {
+template <typename Real, typename Alloc>
+__global__ void call_integrateTo(double dt, mirror::managed_vector<Particle*> const* particles, DataStore<Real, Alloc> * store) {
     integrateTo_gpu(dt, particles, store);
 }
 
-void print_particle_state(std::ostream & out, DataStore const* store) {
+template <typename Real, typename Alloc>
+void print_particle_state(std::ostream & out, DataStore<Real, Alloc> const* store) {
     for (size_t i = 0; i < store->number_of_particles(); ++i) {
         out << store->x(i) << ", ";
         out << store->y(i) << ", ";
@@ -202,6 +273,7 @@ void print_headers(std::ostream & out, size_t nParticles) {
     out << std::endl;
 }
 
+template <typename Real = double, typename Alloc = mirror::managed_allocator<Real>>
 class simulation {
 public:
 
@@ -216,7 +288,7 @@ public:
         device_particles = new mirror::polymorphic_mirror<Particle>(host_particles);
 
         nParticles = host_particles.size();
-        store = new DataStore(nParticles);
+        store = new DataStore<Real, Alloc>(nParticles);
         if(setupProblem)
             store->setup_random_distribution();
     }
@@ -234,15 +306,21 @@ public:
     mirror::host_vector<Particle*> host_particles;
     mirror::polymorphic_mirror<Particle> * device_particles;
     std::pair<double, std::vector<double>> last_checkpoint;
-    DataStore * store = nullptr;
+    DataStore<Real, Alloc> * store = nullptr;
 };
 
 
 TEST_CASE("Simple particle trajectory benchmarks", "[particleSim][benchmark]") {
     
+#ifdef NDEBUG
     size_t const nParticles = 1000000;
     size_t const nIterations = 10000;
     size_t const checkpointInterval = 1000;
+#else
+    size_t const nParticles = 10;
+    size_t const nIterations = 10;
+    size_t const checkpointInterval = 5;
+#endif
 
     auto const nBlocks = 256;
     auto const nThreadsPerBlock = 256;
@@ -250,11 +328,11 @@ TEST_CASE("Simple particle trajectory benchmarks", "[particleSim][benchmark]") {
     double currentTime = 0;
 
     BENCHMARK("Setup Problem (polymorphic mirroring)") {
-        simulation sim(nParticles, false);
+        simulation<double> sim(nParticles, false);
     }
 
     {
-        simulation sim(nParticles);
+        simulation<double> sim(nParticles);
         BENCHMARK("GPU with UMA") {
             for (size_t iteration = 0; iteration < nIterations; ++iteration) {
                 currentTime += dt;
@@ -269,7 +347,37 @@ TEST_CASE("Simple particle trajectory benchmarks", "[particleSim][benchmark]") {
     }
 
     {
-        simulation sim(nParticles);
+        simulation<double, mirror::device_allocator<double>> sim(nParticles);
+        BENCHMARK("GPU with manual sync") {
+            for (size_t iteration = 0; iteration < nIterations; ++iteration) {
+                currentTime += dt;
+                call_integrateTo<<<nBlocks, nThreadsPerBlock>>>(dt, sim.device_particles->get(), sim.store);
+
+                if (iteration % checkpointInterval == 0) {
+                    mirror_sync;
+                    sim.checkpoint(currentTime);
+                }
+            }
+        }
+    }
+
+    {
+        simulation<double> sim(nParticles);
+        BENCHMARK("GPU with UMA by Warp") {
+            for (size_t iteration = 0; iteration < nIterations; ++iteration) {
+                currentTime += dt;
+                call_integrateTo_byWarp<<<nBlocks, nThreadsPerBlock>>>(dt, sim.device_particles->get(), sim.store);
+
+                if (iteration % checkpointInterval == 0) {
+                    mirror_sync;
+                    sim.checkpoint(currentTime);
+                }
+            }
+        }
+    }
+
+    {
+        simulation<double> sim(nParticles);
         BENCHMARK("CPU with OMP") {
             for (size_t iteration = 0; iteration < nIterations; ++iteration) {
                 currentTime += dt;
